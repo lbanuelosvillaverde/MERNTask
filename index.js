ae95e6b16f6a9532e120d1fe3bdc44fc8a0bd762
Luis Bañuelos Villaverde <luisbanuelosvillaverde@MacBook-Air-de-Teresa.local>
2020-07-06 19:02:07 -0500

    Ajuestes para deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({extended: true}));
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -26,6 +26,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 
 //Arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
